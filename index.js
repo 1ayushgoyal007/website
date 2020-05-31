d64c0a8d5c4c44fb3771881bec2739eafe0026a8
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-05-31 19:38:52 +0530

    Added New Pages

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ app.get('/',function(req,res){
     res.render('index');
 });
 
+app.get('/about',function(req,res){
+    res.render('about');
+})
+
+
+app.get('/login',function(req,res){
+    res.render('login');
+});
+
+app.get('/sign-up',function(req,res){
+    res.render('sign-up');
+})
+
+app.get('/contact',function(req,res){
+    res.render('contact');
+})
+
+app.post('/contact',function(req,res){
+    var contact = {
+           name: req.body.username,
+           email: req.body.email,
+           phone: req.body.number,
+           message:req.body.message
+    }
+    res.send('you did it.')
+})
+
+
+
+
+
+app.get('*',function(req,res){
+    res.render('default');
+});
 
 app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), function(req,res){
