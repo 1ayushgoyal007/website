d61b3f4114fc8f16f7a963713cd86dd39c8fed15
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-05-31 20:06:49 +0530

    Adding Email Auth Feature

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs"); 
 app.use(express.static(__dirname+"/public"));
+var User = require('./models/user');
+
+
+
+//Auth Implementation--------------------------------------------
+var passport = require('passport');
+var localStrategy = require("passport-local");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/startup",{ useUnifiedTopology: true,useNewUrlParser:true });
+
+
+app.use(require("express-session")({
+    secret:"Once Again I am Here!!",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
+
 
 
 app.get('/',function(req,res){
@@ -15,14 +44,44 @@ app.get('/about',function(req,res){
     res.render('about');
 })
 
+app.get('/sign-up',function(req,res){
+    res.render('sign-up');
+})
+
+app.post('/sign-up',function(req,res){
+    var newUser = new User({ username:req.body.username,name:req.body.name});
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            return res.render('sign-up');
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/");
+            });
+        }
+    })
+})
 
 app.get('/login',function(req,res){
     res.render('login');
 });
 
-app.get('/sign-up',function(req,res){
-    res.render('sign-up');
+app.post('/login',passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect:'back'
+}),function(req,res){
+    //DO NOTHING
+});
+
+
+app.get('/logout',function(req,res){
+    req.logOut();
+    res.redirect('/');
 })
+
+
+
+
 
 app.get('/contact',function(req,res){
     res.render('contact');
