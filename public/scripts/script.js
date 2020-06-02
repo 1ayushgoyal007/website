c6eb6bbe25207e88aa912b6b946eff25a3c01d8e
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-06-02 13:45:42 +0530

    Fixing Navbar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,33 @@
 
-      mybutton = document.querySelector("#myBtn");
-      console.log(mybutton);
+var mybutton = document.querySelector("#myBtn");
+
+var counters = document.querySelectorAll(".count");
+var speed = 200;
+
+counters.forEach(counter => {
+  const updateCount = () =>{
+    const target = +counter.getAttribute('data-target');
+
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    console.log(count);
+
+    
+    if(count < target){
+      counter.innerText = count+inc;
+      setTimeout(updateCount,1);
+
+    }else{
+      count.innerText = target;
+    }
+  }
+
+  updateCount();
+})
+
+
       window.onscroll = function(){
         console.log(document.body.scrollTop)
         if(document.body.scrollTop > 30 ){
