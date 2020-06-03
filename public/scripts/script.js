0b4171b027db9925bd02498dcdea6c7ea89518a5
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-06-03 22:28:02 +0530

    About Us Page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,38 +4,46 @@ var mybutton = document.querySelector("#myBtn");
 var counters = document.querySelectorAll(".count");
 var speed = 200;
 
-counters.forEach(counter => {
-  const updateCount = () =>{
-    const target = +counter.getAttribute('data-target');
-
-    const count = +counter.innerText;
-
-    const inc = target / speed;
-
-    console.log(count);
-
-    
-    if(count < target){
-      counter.innerText = count+inc;
-      setTimeout(updateCount,1);
-
-    }else{
-      count.innerText = target;
-    }
-  }
-
-  updateCount();
-})
 
 
       window.onscroll = function(){
-        console.log(document.body.scrollTop)
+        console.log(document.body.scrollTop);
+
+        if(counters && document.body.scrollTop > 700){
+          counters.forEach(counter => {
+            const updateCount = () =>{
+              const target = +counter.getAttribute('data-target');
+          
+              const count = +counter.innerText;
+          
+              const inc = target / speed;
+          
+              console.log(count);
+          
+              
+              if(count < target){
+                counter.innerText = count+inc;
+                setTimeout(updateCount,1);
+          
+              }else{
+                count.innerText = target;
+              }
+            }
+          
+            updateCount();
+          })
+          
+        }
         if(document.body.scrollTop > 30 ){
           mybutton.style.display = "block";
         }else{
           mybutton.style.display = "none";
         }
       }
+
+
+
+
 
       function topFunction(){
         document.body.scrollTop = 0;
